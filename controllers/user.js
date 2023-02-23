bbf79261a6197a00087064beceed0c57c4a36069
rollandnicolas <rollandnpro@gmail.com>
2023-02-23 12:18:33 +0100

    password validator ok, probleme de connexion

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
+const passwordValidator = require("password-validator")
 const User = require('../models/User');
+
+// Schema for passwordValidator
+var schema = new passwordValidator();
+schema
+  .is()
+  .min(6)
+  .is()
+  .max(20)
+  .has()
+  .not()
+  .spaces()
+  .has()
+  .uppercase()
+  .has()
+  .lowercase()
+  .has()
+  .digits(1);
+
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -11,35 +30,73 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
 
-
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    User.findOne({ email: req.body.email }) 
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({
+            message: "Aucun compte ne correspond à l'adresse email renseingée !", 
+          });
+        }
+        bcrypt
+          .compare(req.body.password, user.password) 
+          .then((valide) => {
+            if (!valide) {
+              return res
+                .status(401)
+                .json({ message: "Mot de passe incorrect !" });
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+  
+            const newToken = jwt.sign(
+              { userId: user._id },
+              process.env.JWT_SECRET_KEY,
+              { expiresIn: "24h" }
+            );
+  
+            res.status(200).json({ userId: user._id, token: newToken });
+          })
+  
+          .catch((error) => res.status(401).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  };
+
+/*exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user) => {
+        if (!user) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                }
+
+                                res.status(200).json({
+                                    userId: user._id,
+                                    token: jwt.sign(
+                                        { userId: user._id },
+                                           process.env.JWT_SECRET_KEY ,
+                                        { expiresIn: '24h' }
+                                    ),
+                                });
+                            })
+                            .catch(error => res.status(500).json({ error }));
                     }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        ),
-                       // message: 'Utilisateur connecté !'
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
- };
+                const newToken = jwt.sign(
+                    { userId: user._id },
+                    process.env.JWT_SECRET_KEY,
+                    { expiresIn: "24h" }
+                );
+
+                res.status(200).json({ userId: user._id, token: newToken });
+            })
+            .catch(error => res.status(500).json({ error }));
+    };*/
