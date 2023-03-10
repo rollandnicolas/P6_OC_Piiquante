3a8af388a06e1537a6f418adc090a3a6025c4bb8
rollandnicolas <rollandnpro@gmail.com>
2023-03-10 09:25:40 +0100

    Version finale corrigée et testée

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -50,13 +50,13 @@ exports.likeSauceById = (req, res, next) => {
   if (req.body.like === 1) {
     // On ajoute 1 like et on l'envoie dans le tableau "usersLiked"
     Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } })
-      .then((sauce) => res.status(200).json({ message: 'Like ajouté !' }))
+      .then(() => res.status(200).json({ message: 'Like ajouté !' }))
       .catch(error => res.status(400).json({ error }));
   }
   // Si l'utilisateur n'aime pas la sauce
   else if (req.body.like === -1) {
     Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } })
-      .then((sauce) => res.status(200).json({ message: 'Dislike ajouté !' }))
+      .then(() => res.status(200).json({ message: 'Dislike ajouté !' }))
       .catch(error => res.status(400).json({ error }));
   }
 else {
@@ -65,13 +65,13 @@ else {
         if (sauce.usersLiked.includes(req.body.userId)) {
           // On enlève un like du tableau "userLiked" 
           Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
-            .then((sauce) => { res.status(200).json({ message: 'Like supprimé !' }) })
+            .then(() => { res.status(200).json({ message: 'Like supprimé !' }) })
             .catch(error => res.status(400).json({ error }))
         }
         else if (sauce.usersDisliked.includes(req.body.userId)) {
           // On enlève un dislike du tableau "userDisliked" 
           Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
-            .then((sauce) => { res.status(200).json({ message: 'Dislike supprimé !' }) })
+            .then(() => { res.status(200).json({ message: 'Dislike supprimé !' }) })
             .catch(error => res.status(400).json({ error }))
         }
       })
--- middlewares/restrictLikes.js
+++ middlewares/restrictLikes.js
@@ -1,12 +1,9 @@
-const jwt = require("jsonwebtoken");
 const Sauce = require("../models/sauce");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedtoken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const userId = decodedtoken.userId;
-    Sauce.findOne({ id: req.params.id })
+    const userId = req.body.userId;
+    Sauce.findOne({ _id: req.params.id })
       .then((sauce) => {
         if (
           (sauce.usersLiked.every((user) => user !== userId) && // si tous les usersLiked & usersDesliked ne contiennent pas l'userId
@@ -18,7 +15,7 @@ module.exports = (req, res, next) => {
           sauce.usersLiked.includes(userId) || // si l'userId est dans usersLiked
           sauce.usersDisliked.includes(userId) // si l'userId est dans usersDisliked
         ) {
-          res.status(403).json({ message: "You can't like or dislike twice" });
+          res.status(403).json({ message: "Vous ne pouvez pas liker ou disliker une même sauce plusieurs fois" });
         }
       })
       .catch((error) => console.log(error));
